c5313a8e718efce3d083a87365f0407a179ae653
mista-luna <lunagangel1005@gmail.com>
2026-09-21 17:29:09 -0700

    update adder.4v fix

--- src/adder4.v
+++ src/adder4.v
@@ -14,10 +14,10 @@ module adder4(
    
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
 	wire cout0;
-	fulladd addc0 (.x(a[0]) , .y(b[0]) , .cin(1'b0000) , .sum(result[0]), .cout(cout0));
-	fulladd addc1 (.x(a[1]) , .y(b[1]) , .cin(1'b0001) , .sum(result[1]), .cout(cout1));
-	fulladd addc1 (.x(a[2]) , .y(b[2]) , .cin(1'b0010) , .sum(result[2]), .cout(cout2));
-	fulladd addc1 (.x(a[3]) , .y(b[3]) , .cin(1'b0011) , .sum(result[3]), .cout(cout3));
+	fulladd addc0 (.x(a[0]) , .y(b[0]) , .cin(1'b00) , .sum(result[0]), .cout(cout0));
+	fulladd addc1 (.x(a[1]) , .y(b[1]) , .cin(1'b01) , .sum(result[1]), .cout(cout1));
+	fulladd addc2 (.x(a[2]) , .y(b[2]) , .cin(1'b10) , .sum(result[2]), .cout(cout2));
+	fulladd addc3 (.x(a[3]) , .y(b[3]) , .cin(1'b11) , .sum(result[3]), .cout(cout3));
    
 endmodule
 
